c9d0a14cc400fc6574d5ad2bfbbe4a573db467ea
Mazen <mazenabougamrah@gmail.com>
2026-06-24 06:24:59 +0100

    function bug fix

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
-def calculate_required_length(Plate):
+def calculate_required_length(plate):
     # This function is used to avoid 
     # hardcoding any lengths in the 
     # constraints and to ensure modularity.
     # An extra fixed loss of 0.5m is assumed
     # when a slab is rolled for a plate.
-    return Plate.length + 0.5
+    return plate.length + 0.5
